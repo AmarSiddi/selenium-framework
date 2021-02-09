1de7dbd6a22e22f7f72a59373249a571d6c79c1d
amarsiddi <amarnathsiddi@gmail.com>
2021-02-09 17:24:50 +0000

    first test commit

---
+++ src/test/java/com/framework/tests/TestClass.java
@@ -0,0 +1,25 @@
+package com.framework.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+    @Test
+    public void test(){
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+        driver.manage().window().maximize();
+
+
+        driver.quit();
+
+    }
+
+
+}
